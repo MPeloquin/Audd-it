6072ee494a0daa0aed4df880794fa83da71409bc
Maxime Péloquin <maxime.peloquin@gmail.com>
2013-09-01 05:54:08 -0400

    Fix nil value exception

--- Auditor/BeltBuckleAuditor.lua
+++ Auditor/BeltBuckleAuditor.lua
@@ -3,11 +3,10 @@ BeltBuckleAuditor.New = function()
 	local self = {}
 
   self.ExtraGemAuditor = ExtraGemAuditor.New()
-  self.ItemLevelInspector = ItemLevelInspector.New()
   
   self.Audit = function()    
-    local itemLevel = self.ItemLevelInspector.GetItemLevel("WaistSlot")
-    local playerLevel = UnitLevel("player")
+    local itemLevel = ItemLevelInspector.New().GetItemLevel("WaistSlot") or 0
+    local playerLevel = UnitLevel("player") or 0
     
     if (itemLevel >= 417 and playerLevel >= 85) then
       self.ExtraGemAuditor.Audit("WaistSlot", "Missing Living Steel Belt Buckle")
--- Auditor/Profession/BlacksmithingAuditor.lua
+++ Auditor/Profession/BlacksmithingAuditor.lua
@@ -1,19 +1,19 @@
 BlacksmithingAuditor = {}
 BlacksmithingAuditor.New = function()
 	local self = {}
-  
-  self.ExtraGemAuditor = ExtraGemAuditor.New()
-  
-  self.Audit = function()
-    local blacksmithingLevel = ProfessionInspector.New().GetBlackSmithingLevel()
-    
-    if(blacksmithingLevel == -1) then
+
+	self.ExtraGemAuditor = ExtraGemAuditor.New()
+
+	self.Audit = function()
+	local blacksmithingLevel = ProfessionInspector.New().GetBlackSmithingLevel()
+
+		if(blacksmithingLevel == -1) then
 			return
 		end    
-       
-    self.ExtraGemAuditor.Audit("WristSlot", "Blacksmithing: Missing socket")
-    self.ExtraGemAuditor.Audit("HandsSlot", "Blacksmithing: Missing socket")
-  end
-  
-  return self
+
+		self.ExtraGemAuditor.Audit("WristSlot", "Blacksmithing: Missing socket")
+		self.ExtraGemAuditor.Audit("HandsSlot", "Blacksmithing: Missing socket")
+	end
+
+	return self
 end
