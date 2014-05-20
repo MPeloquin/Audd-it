05dca18d2e71ce912c8601c79a4af06b37601638
Maxime Péloquin <maxime.peloquin@gmail.com>
2014-05-20 17:07:22 -0400

    Fix null bug when the player doesn't have any gear

--- Auditor/Inspector/ItemLevelInspector.lua
+++ Auditor/Inspector/ItemLevelInspector.lua
@@ -3,9 +3,10 @@ ItemLevelInspector.New = function()
 	local self = {}
   
   self.GetItemLevel = function(itemName)
+    
     local itemId = GetInventorySlotInfo(itemName)
-    local itemLink = GetInventoryItemLink("player", itemId)
-   
+    local itemLink = GetInventoryItemLink("player", itemId) or 0
+     
     local itemLevel = select(4, GetItemInfo(itemLink));
     
     return itemLevel
